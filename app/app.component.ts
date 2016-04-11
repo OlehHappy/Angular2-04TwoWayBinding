73733ed580c5ed347fb71ab4aed6252da5fc044b
Admin <olegtranslator@gmail.com>
2016-04-11 07:56:19 +0200

    input bunding title

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <input type="text" [value]="title" (input)="title = $event.target.value" />
+        <input type="text" [(ngModel)]="title" />
+        <input type="text" bindon-ngModel="title" />
+
+        <input type="button" (click)="title = ''" value="Clear" />
+        Preview: {{title}}
+    `
+})
+export class AppComponent {
+    title = "Angular App";
+}
